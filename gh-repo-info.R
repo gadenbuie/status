c990318dff6afc78339d16d5bdc41d189e8ac5e0
Garrick Aden-Buie <garrick@adenbuie.com>
2020-12-17 22:43:00 -0500

    Add feature to display all public, non-fork repos by the account owner
    - Renamed YAML key for params -> repos

--- gh-repo-info.R
+++ gh-repo-info.R
@@ -10,8 +10,11 @@ library(readr)
 
 # gh functions ----
 gh_workflows <- memoise::memoise(function(owner, repo, ...) {
-  gh("/repos/{owner}/{repo}/actions/workflows", owner = owner, repo = repo) %>%
-    .$workflows
+  tryCatch(
+    gh("/repos/{owner}/{repo}/actions/workflows", owner = owner, repo = repo) %>%
+      .$workflows,
+    error = function(e) NULL
+  )
 }, cache = cache_memory())
 
 gh_runs <- memoise::memoise(function(owner, repo, workflow_id, ...) {
@@ -28,40 +31,83 @@ gh_url <- memoise::memoise(function(url) {
   gh(url)
 }, cache = cache_memory())
 
+gh_repo <- memoise::memoise(function(owner, repo, ...) {
+  gh("/repos/{owner}/{repo}", owner = owner, repo = repo)
+}, cache = cache_memory())
 
-# Get repos
-gh_get_repo_status <- function(repos) {
-  repos <- map_dfr(repos, ~ tibble(repo = .), .id = "owner")
+gh_my_repos <- memoise::memoise(function() {
+  gh("/user/repos", .limit = Inf, visibility = "public", affiliation = "owner") %>%
+    map(keep, negate(is.null)) %>%
+    map(keep, negate(is.list)) %>%
+    map_dfr(as_tibble) %>%
+    filter(!private, !fork) %>%
+    mutate(owner = gh_whoami()$login) %>%
+    select(owner, repo = name, full_name, contains("count"), html_url_repo = html_url, fork) %>%
+    arrange(desc(stargazers_count))
+}, cache = cache_memory())
 
-  repos$workflows <- repos %>% pmap(gh_workflows)
-  repos <- repos %>%
-    unnest(workflows) %>%
+gh_repo_stats <- function(repos) {
+  # repos should be a tibble now with owner, repo
+  repos$.repo <- pmap(repos, gh_repo)
+  repos %>%
     mutate(
-      workflow_id = map_chr(workflows, "id"),
-      badge_url = map_chr(workflows, "badge_url")
-    )
-
-  repos$runs <- pmap(repos, gh_runs)
-  repos <- repos %>%
-    mutate(
-      html_url_run = map_chr(runs, "html_url"),
-      run_conclusion = map_chr(runs, "conclusion"),
-      commit_message = map_chr(runs, ~ .x$head_commit$message),
-      commit_id = map_chr(runs, `[[`, c("head_commit", "id")),
-      repo_name = map_chr(runs, `[[`, c("head_repository", "full_name")),
-      html_url_repo = map_chr(runs, `[[`, c("head_repository", "html_url")),
-      .repo = map(map_chr(runs, `[[`, c("head_repository", "url")), gh_url),
+      full_name = map_chr(.repo, "full_name"),
       stargazers_count = map_dbl(.repo, "stargazers_count"),
       watchers_count = map_dbl(.repo, "watchers_count"),
       open_issues_count = map_dbl(.repo, "open_issues_count"),
       forks_count = map_dbl(.repo, "forks_count"),
-      open_issues_count = map_dbl(.repo, "open_issues_count")
+      open_issues_count = map_dbl(.repo, "open_issues_count"),
+      fork = map_lgl(.repo, "fork"),
+      html_url_repo = map_chr(.repo, "html_url")
+    )
+}
+
+gh_repo_workflows <- function(repos) {
+  # repos should be a tibble with owner, repo
+  repos$.workflows <- repos %>% pmap(gh_workflows)
+
+  workflows <- repos %>%
+    unnest(.workflows) %>%
+    mutate(
+      workflow_id = map_chr(.workflows, "id"),
+      badge_url = map_chr(.workflows, "badge_url")
     )
 
-  repos %>%
-    select_if(negate(is.list)) %>%
-    arrange(repo_name) %>%
-    write_csv("repos.csv")
+  workflows$runs <- pmap(workflows, gh_runs)
+  workflows %>%
+    mutate(
+      html_url_run = map_chr(runs, "html_url"),
+      run_conclusion = map_chr(runs, "conclusion"),
+      commit_message = map_chr(runs, ~ .x$head_commit$message),
+      commit_id = map_chr(runs, `[[`, c("head_commit", "id"))
+    )
+}
+
+# Get repos
+gh_get_repo_status <- function(repo_list, all_by_owner = FALSE, .write_csv = !interactive()) {
+  repos <- map_dfr(repo_list, ~ tibble(repo = .), .id = "owner")
+
+  repos <- gh_repo_stats(repos)
+  by_vars <- c("owner", "repo")
+
+  if (all_by_owner) {
+    owner_repos <- gh_my_repos() %>% anti_join(repos, by = by_vars)
+    repos <- bind_rows(repos, owner_repos)
+  }
+
+  workflows <- repos %>% select(owner, repo) %>% gh_repo_workflows()
+
+  repos <- bind_rows(
+    inner_join(repos, workflows, by = by_vars),
+    anti_join(repos, workflows, by = by_vars)
+  )
+
+  if (isTRUE(.write_csv)) {
+    repos %>%
+      select_if(negate(is.list)) %>%
+      arrange(full_name) %>%
+      write_csv("repos.csv")
+  }
 
   repos
 }
