e2da33e7f73bb1bf2a130347acccf0cb0906cffa
Garrick Aden-Buie <garrick@adenbuie.com>
2022-04-13 17:14:55 -0400

    Collapse badges together, only push/scheduled events
    Closes #6

--- gh-repo-info.R
+++ gh-repo-info.R
@@ -77,6 +77,7 @@ gh_repo_workflows <- function(repos) {
   workflows$runs <- pmap(workflows, gh_runs)
   workflows %>%
     mutate(
+      event = map_chr(runs, "event"),
       html_url_run = map_chr(runs, "html_url"),
       run_conclusion = map_chr(runs, "conclusion", .default = NA_character_),
       commit_message = map_chr(runs, ~ .x$head_commit$message),
@@ -111,6 +112,14 @@ gh_get_repo_status <- function(
   }
 
   workflows <- repos %>% select(owner, repo) %>% gh_repo_workflows()
+
+  workflows <-
+    workflows %>%
+    filter(event %in% c("push", "scheduled")) %>%
+    mutate(badge = glue::glue("[![]({badge_url})]({html_url_run})")) %>%
+    group_by(owner, repo, commit_id, commit_message) %>%
+    summarize(badge = paste(badge, collapse = " ")) %>%
+    ungroup()
 
   repos <- bind_rows(
     inner_join(repos, workflows, by = by_vars),
